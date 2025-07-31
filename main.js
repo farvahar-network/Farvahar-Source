14e9060cc01e63c2ffdaaa573678bb70a5622b7d
Ytext <ytextdream@gmail.com>
2025-07-31 13:24:53 +0330

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,62 @@
 const { app, BrowserWindow, ipcMain, Menu } = require('electron');
-const { spawn, exec } = require('child_process');
+const { exec, spawn } = require('child_process');
 const path = require('path');
+const os = require('os');
+
+const gameServers = {
+  fortnite: 'fortnite.com',
+  valorant: 'riotgames.com',
+  rust: 'playrust.com',
+  cod: 'callofduty.com',
+  warfare: 'warfare.io',
+  hypixel: 'mc.hypixel.net',
+  mojang: 'mojang.com',
+  minecraft: 'minecraft.net',
+  roblox: 'roblox.com',
+  apex: 'ea.com',
+  cs2: 'cs2game.dev',
+  dota2: 'dota2.com',
+  lol: 'leagueoflegends.com',
+  pubg: 'pubg.com',
+  overwatch: 'overwatch.blizzard.com',
+  steam: 'store.steampowered.com',
+  epic: 'epicgames.com',
+  blizzard: 'blizzard.com',
+  ubisoft: 'ubisoft.com',
+  origin: 'origin.com',
+  riot: 'riotgames.com',
+  xbox: 'xbox.com',
+  psn: 'playstation.com',
+  nintendo: 'nintendo.com',
+  battlenet: 'battle.net',
+  tarkov: 'escapefromtarkov.com',
+  destiny2: 'bungie.net',
+  warframe: 'warframe.com',
+  arma3: 'arma3.com',
+  gta5: 'rockstargames.com',
+  rockstar: 'rockstargames.com',
+  dayz: 'dayz.com',
+  rainbow: 'rainbow6.com',
+  payday: 'paydaythegame.com',
+  eldenring: 'eldenring.com',
+  cyberpunk: 'cyberpunk.net',
+  palworld: 'pocketpair.jp',
+  terraria: 'terraria.org',
+  starfield: 'bethesda.net',
+  hl2: 'valvesoftware.com',
+  gmod: 'gmod.com',
+  tf2: 'teamfortress.com',
+  osu: 'ppy.sh',
+  slime: 'slimerancher.com',
+  amongus: 'innersloth.com',
+  phasmo: 'kineticgames.co.uk',
+  league: 'leagueoflegends.com',
+  diablo: 'diablo4.blizzard.com',
+  wow: 'worldofwarcraft.com',
+  smite: 'smitegame.com',
+  quake: 'quake.com',
+  doom: 'doom.com'
+};
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -18,14 +74,38 @@ function createWindow() {
   });
 
   Menu.setApplicationMenu(null);
-
   win.setMenuBarVisibility(false);
   win.loadFile('main.html');
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  optimizeSystemPerformance();
+});
 
-ipcMain.handle('ping-server', async (event, ipAddress) => {
+function optimizeSystemPerformance() {
+  if (process.platform === 'win32') {
+    exec('ipconfig /flushdns');
+    exec('netsh winsock reset');
+    exec('netsh int ip reset');
+    exec('netsh interface ipv4 reset');
+    exec('netsh interface ipv6 reset');
+    exec('netsh advfirewall reset');
+    exec('del /f /s /q %temp%\\*');
+    exec('del /f /s /q C:\\Windows\\Temp\\*');
+    exec('cls');
+  } else if (process.platform === 'darwin' || process.platform === 'linux') {
+    exec('dscacheutil -flushcache');
+    exec('sudo killall -HUP mDNSResponder');
+    exec('sudo sysctl -w net.inet.tcp.delayed_ack=0');
+    exec('sudo rm -rf /Library/Caches/*');
+    exec('sudo rm -rf ~/Library/Caches/*');
+    exec('sudo rm -rf /System/Library/Caches/*');
+    exec('sudo rm -rf /private/var/folders/*');
+  }
+}
+
+function pingHost(ipAddress) {
   return new Promise((resolve) => {
     const cmd = process.platform === 'win32'
       ? `ping -n 1 ${ipAddress}`
@@ -37,5 +117,64 @@ ipcMain.handle('ping-server', async (event, ipAddress) => {
       resolve(match ? `${match[1]} ms` : 'N/A');
     });
   });
+}
+
+ipcMain.handle('ping-server', async (event, ipAddress) => {
+  return await pingHost(ipAddress);
 });
 
+ipcMain.handle('ping-all-games', async () => {
+  const results = {};
+  const keys = Object.keys(gameServers);
+
+  for (const game of keys) {
+    const domain = gameServers[game];
+    const result = await pingHost(domain);
+    results[game] = result;
+  }
+
+  return results;
+});
+
+ipcMain.handle('clear-network-cache', async () => {
+  return new Promise((resolve) => {
+    if (process.platform === 'win32') {
+      exec('ipconfig /flushdns && netsh winsock reset', (err) => {
+        resolve(err ? 'Failed' : 'Done');
+      });
+    } else if (process.platform === 'darwin' || process.platform === 'linux') {
+      exec('sudo killall -HUP mDNSResponder', (err) => {
+        resolve(err ? 'Failed' : 'Done');
+      });
+    } else {
+      resolve('Unsupported');
+    }
+  });
+});
+
+ipcMain.handle('boost-performance', async () => {
+  return new Promise((resolve) => {
+    if (process.platform === 'win32') {
+      exec('del /f /s /q %temp%\\* && cls', (err) => {
+        resolve(err ? 'Fail khord' : 'ok shod');
+      });
+    } else if (process.platform === 'darwin' || process.platform === 'linux') {
+      exec('rm -rf ~/Library/Caches/*', (err) => {
+        resolve(err ? 'Fail khord' : 'ok shod');
+      });
+    } else {
+      resolve('Unsupported');
+    }
+  });
+});
+
+ipcMain.handle('get-system-info', () => {
+  return {
+    platform: os.platform(),
+    arch: os.arch(),
+    cpus: os.cpus().length,
+    freemem: os.freemem(),
+    totalmem: os.totalmem(),
+    uptime: os.uptime()
+  };
+});
